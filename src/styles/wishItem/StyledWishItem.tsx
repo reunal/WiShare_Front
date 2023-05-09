52563d3c7574c03a37ecfd40610a960930f8f869
Moyujin <scab12341@gmail.com>
2023-05-09 21:19:30 +0900

    위시아이템 추가

---
+++ src/styles/wishItem/StyledWishItem.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const StyledWishItem = styled.div`
+  width: 100%;
+  height: 80px;
+  border: solid 1px #e5e5e5;
+  border-radius: 10px;
+  padding: 10px;
+  margin-top: 50px;
+`;
+
+export const StyledItemInfo = styled.div`
+  div {
+    float: left;
+  }
+
+  div:nth-child(2) {
+    margin-left: 13px;
+  }
+
+  div:last-child {
+    float: right;
+    margin-right: 10px;
+    line-height: 60px;
+  }
+
+  img {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    border: solid 1px red;
+  }
+
+  h3 {
+    font-size: 24px;
+  }
+
+  h6 {
+    color: #333;
+    font-size: 20px;
+    margin-top: 10px;
+  }
+
+  p {
+    color: #666;
+    font-size: 12px;
+  }
+`;
