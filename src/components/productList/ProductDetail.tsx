49ff10ed9da6b53e94d5873f0eeea16fd18de864
susung1107 <susung1107@gmail.com>
2023-05-10 14:10:52 +0900

    상품상세보기페이지스타일링

--- src/components/productList/ProductDetail.tsx
+++ src/components/productList/ProductDetail.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import { Grid } from '@mui/material';
+import { Grid, Button, ButtonGroup } from '@mui/material';
+import noImage from '../../asset/images/noImage.jpeg';
+import { DetailImage, DetailContent, DetailUnderLine } from '../../styles/product/StyledProduct';
 
 const ProductDetail = (): JSX.Element => {
 	return (
 		<Grid container spacing={2}>
 			<Grid item xs />
 			<Grid item xs={5}>
-				<img src="" alt="" />
+				<DetailImage>
+					<img src={noImage} alt="" />
+				</DetailImage>
 			</Grid>
 			<Grid item xs={5}>
-				<h3>MLB 캡 모자</h3>
-				<p>MLB</p>
+				<DetailContent>
+					<h3>MLB 캡 모자</h3>
+					<p>MLB</p>
 
-				<h2>39,000 원</h2>
+					<h2>39,000 원</h2>
+					<DetailUnderLine />
+					<div>
+						<ButtonGroup>
+							<Button>위시하기</Button>
+							<Button>선물하기</Button>
+						</ButtonGroup>
+					</div>
+				</DetailContent>
 			</Grid>
 			<Grid item xs />
 		</Grid>
--- src/styles/product/StyledProduct.tsx
+++ src/styles/product/StyledProduct.tsx
@@ -10,3 +10,43 @@ export const FilterSelector = styled.select`
 	font-size: 11px;
 	float: right;
 `;
+
+export const DetailImage = styled.div`
+	margin-top: 100px;
+	width: 100%;
+	height: auto;
+
+	img {
+		width: 100%;
+		height: 100%;
+		border: solid 1px #e5e5e5;
+	}
+`;
+
+export const DetailContent = styled.div`
+	margin-top: 150px;
+	h3 {
+		font-size: 24px;
+		color: #333;
+	}
+
+	p {
+		color: #666;
+		font-size: 14px;
+	}
+
+	h2 {
+		font-size: 30px;
+		margin-top: 50px;
+	}
+
+	div {
+		margin-top: 10px;
+	}
+`;
+
+export const DetailUnderLine = styled.div`
+	width: 100%;
+	height: 1px;
+	background: #e5e5e5;
+`;
